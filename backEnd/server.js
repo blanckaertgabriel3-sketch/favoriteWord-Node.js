21f1eaa6da3a43514aa7dfc795d4ac6c632c5196
blanckaertgabriel3-sketch <blanckaertgabriel3@gmail.com>
2026-04-05 15:19:20 +0200

    connection to mysql

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const mysql = require("mysql");
 const port = process.env.PORT || 5000;
+
 const app = express();
+
+const connexion = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'root',
+	database: 'favoriteword'
+});
+connexion.connect((err) => {
+	if(err) {
+		console.error("mysql connection error : " + err.stack);
+		return;
+	}
+	console.log("Connected to myql");
+})
 
 app.get("/", (req, res) => {
 	res.status(200).json({
@@ -9,7 +25,9 @@ app.get("/", (req, res) => {
 		id: 789
 	});
 })
+const users = require("./routes/users");
+app.use("/users", users);
 
 app.listen(port, () => {
-	console.log("Server is oppen");
+	console.log("Server is open");
 })
