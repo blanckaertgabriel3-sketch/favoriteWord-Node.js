35ba79d99ff7d60fcd78bda28ac960e4c8f6821f
blanckaertgabriel3-sketch <blanckaertgabriel3@gmail.com>
2026-04-06 11:45:15 +0200

    change db connexion

--- backEnd/routes/users.js
+++ backEnd/routes/users.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const pool = require("../pool");
 
 router.get("/", (req, res) => {
-	res.status(200).json({
-		message: "All users!"
-	})
-})
+  pool.query("SELECT * FROM users", (err, rows, fields) => {
+    if (err) {
+		throw err;
+	}
+    res.json({ rows });
+  });
+});
 
 module.exports = router;
--- backEnd/server.js
+++ backEnd/server.js
@@ -1,33 +1,20 @@
 const express = require("express");
-const mysql = require("mysql");
-const port = process.env.PORT || 5000;
+const pool = require("./pool");
+const users = require("./routes/users");
 
+const port = process.env.PORT || 5000;
 const app = express();
 
-const connexion = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'root',
-	database: 'favoriteword'
-});
-connexion.connect((err) => {
-	if(err) {
-		console.error("mysql connection error : " + err.stack);
-		return;
-	}
-	console.log("Connected to myql");
-})
-
-app.get("/", (req, res) => {
-	res.status(200).json({
-		name: "gariel", 
-		age:57,
-		id: 789
-	});
-})
-const users = require("./routes/users");
 app.use("/users", users);
 
-app.listen(port, () => {
-	console.log("Server is open");
+pool.getConnection((err, connexion) => {
+	if(err) {	
+		console.log("connexion impossible");
+		process.exit(1);
+	}
+	connexion.release();
+	console.log("Connected to db");
+	app.listen(port, () => {
+	  console.log("Server is open");
+	});
 })
